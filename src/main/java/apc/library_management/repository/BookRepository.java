96b0b65ffaa448266a17a28802c7883f804b6ce5
shivamxx <shivamxx789@gmail.com>
2025-09-14 14:28:04 +0530

    Revert "mysql is added and some changes made"
    This reverts commit 444ec17298e474be83f91397bc3a9db4ed4536e5.

--- src/main/java/apc/library_management/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package apc.library_management.repository;
-
-import apc.library_management.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
